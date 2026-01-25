049ce55edb787e154dd5279b435d8a6f61b08c63
Israt Jahan Jui <isratjahanjui847@gmail.com>
2026-01-25 18:25:50 +0600

    jv
    jj

---
+++ FirstJavaProgram/src/firstjavaprogram/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+
+package firstjavaprogram;
+
+public class FirstJavaProgram {
+
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ JavaSecondClass/src/javasecondclass/JavaSecondClass.java
@@ -0,0 +1,19 @@
+package javasecondclass;
+
+public class JavaSecondClass {
+
+    public static void main(String[] args) {
+
+        float radius;
+        float area;
+        radius = 5;
+
+        area = (float) 3.1416 * radius * radius;
+        System.out.println("Area of a circel is " + area);
+
+        
+        
+        
+    }
+
+}
