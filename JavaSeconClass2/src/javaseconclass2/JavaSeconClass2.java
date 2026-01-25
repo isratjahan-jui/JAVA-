1449c6cf1669c722ac3b4c3e3caf552918b85ad2
Israt Jahan Jui <isratjahanjui847@gmail.com>
2026-01-25 19:03:55 +0600

    jv

---
+++ JavaSeconClass2/src/javaseconclass2/JavaSeconClass2.java
@@ -0,0 +1,28 @@
+
+package javaseconclass2;
+
+import java.util.Scanner;
+
+public class JavaSeconClass2 {
+
+  
+    public static void main(String[] args) {
+ 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter seconds: ");
+
+        int seconds = scanner.nextInt();
+        int minutes = seconds / 60;
+        int second = seconds % 60;
+
+        String minMassage = "minute";
+        if (minutes > 1) {
+            minMassage = " minutes ";
+        } else {
+            minMassage = " minute ";
+        }
+
+        System.out.println(minutes + minMassage   + second + " seconds");
+    }
+    
+}
---
+++ JavaSeconClass3/src/javaseconclass3/JavaSeconClass3.java
@@ -0,0 +1,19 @@
+
+package javaseconclass3;
+
+import java.util.Scanner;
+
+
+public class JavaSeconClass3 {
+
+
+    public static void main(String[] args) {
+         System.out.print("Please enter your name: ");
+
+        String name = input.nextLine();
+        System.out.println("My  name is " + name);
+        
+        Scanner input = new Scanner(System.in);
+    }
+    
+}
