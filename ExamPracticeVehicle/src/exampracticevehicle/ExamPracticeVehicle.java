842a75282b3ee0db07312ab4c2aec5d5e248b54c
Israt Jahan Jui <isratjahanjui847@gmail.com>
2026-02-19 16:08:58 +0600

    19feb

---
+++ ExamPracticeVehicle/src/exampracticevehicle/ExamPracticeVehicle.java
@@ -0,0 +1,17 @@
+
+package exampracticevehicle;
+
+
+public class ExamPracticeVehicle {
+
+
+    public static void main(String[] args) {
+        
+       Truck t =new Truck(2500, 500);
+        System.out.println(t.getSalePrice());
+        
+        
+        
+    }
+    
+}
