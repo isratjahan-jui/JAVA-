2e237ebc82728ecf0182b41ac2b79b50ee8b21de
Israt Jahan Jui <isratjahanjui847@gmail.com>
2026-02-24 20:46:49 +0600

    exam

---
+++ ExamJavaVehicle/src/examjavavehicle/ExamJavaVehicle.java
@@ -0,0 +1,14 @@
+package examjavavehicle;
+
+public class ExamJavaVehicle {
+
+    public static void main(String[] args) {
+        
+          Truck t = new Truck(3000, 500);
+        System.out.println(t.getSalePrice());
+       
+        
+
+    }
+
+}
---
+++ ExamJavaVehicle/src/examjavavehicle/Vehicle.java
@@ -0,0 +1,20 @@
+package examjavavehicle;
+
+public class Vehicle {
+
+    int speed;
+    double regularPrice;
+    String color;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(double regularPrice) {
+        this.regularPrice = regularPrice;
+    }
+
+    public double getSalePrice() {
+        return regularPrice;
+    }
+
+}
