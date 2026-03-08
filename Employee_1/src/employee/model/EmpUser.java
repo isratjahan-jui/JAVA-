abfa0b0970c3382ad67ea5281f7242eb37e491e5
Israt Jahan Jui <isratjahanjui847@gmail.com>
2026-03-08 15:58:12 +0600

    8 march

---
+++ Employee_1/src/employee/model/EmpUser.java
@@ -0,0 +1,60 @@
+package employee.model;
+
+public class EmpUser {
+
+    private int id;
+    private String name;
+    private String email;
+    private double salary;
+
+    public EmpUser() {
+    }
+
+   
+
+    public EmpUser(int id, String name, String email, double salary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpUser{" + "id=" + id + ", name=" + name + ", email=" + email + ", salary=" + salary + '}';
+    }
+
+}
