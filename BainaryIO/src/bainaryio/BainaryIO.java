e8f2fa998e44f3357e03121279b034ae5b2a7bad
Israt Jahan Jui <isratjahanjui847@gmail.com>
2026-03-29 16:52:02 +0600

    */

--- BainaryIO/src/bainaryio/BainaryIO.java
+++ BainaryIO/src/bainaryio/BainaryIO.java
@@ -1,5 +1,6 @@
 package bainaryio;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,8 +12,44 @@ public class BainaryIO {
 
     public static void main(String[] args) {
 
+        File file = new File("hello.dat");
+
         try {
 
+            FileOutputStream output = new FileOutputStream(file);
+
+//            FileOutputStream output = new FileOutputStream("C:\\Users\\Admin\\Desktop\\new.txt");
+            for (int i = 1; i <= 10; i++) {
+                output.write(i);
+
+            }
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BainaryIO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BainaryIO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+
+            FileInputStream input = new FileInputStream(file);
+
+//            FileInputStream input = new FileInputStream("C:\\Users\\Admin\\Desktop\\new.txt");
+            int value;
+            while ((value = input.read()) != -1) {
+                System.out.println(value + " ");
+
+            }
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BainaryIO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BainaryIO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+//       notepad
+        try {
             FileOutputStream output = new FileOutputStream("C:\\Users\\Admin\\Desktop\\new.txt");
 
             for (int i = 1; i <= 10; i++) {
@@ -40,7 +77,8 @@ public class BainaryIO {
         } catch (IOException ex) {
             Logger.getLogger(BainaryIO.class.getName()).log(Level.SEVERE, null, ex);
         }
+//        note.....
+
     }
 
 }
-                                        
